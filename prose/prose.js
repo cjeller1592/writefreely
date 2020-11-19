e1488e21112123d8f3fb825b9a0790444137ff41
matejcsok-istvan <matejcsok.upwork@gmail.com>
2020-11-19 22:21:38 +0100

    pm editor saving issue and word counter fixed

--- prose/prose.js
+++ prose/prose.js
@@ -9,60 +9,88 @@
 //   destroy() { this.textarea.remove() }
 // }
 
-import {EditorView} from "prosemirror-view"
-import {EditorState} from "prosemirror-state"
-import {schema, defaultMarkdownParser, defaultMarkdownSerializer} from "prosemirror-markdown"
-import {exampleSetup} from "prosemirror-example-setup"
+import { EditorView } from "prosemirror-view";
+import { EditorState } from "prosemirror-state";
+import {
+  schema,
+  defaultMarkdownParser,
+  defaultMarkdownSerializer,
+} from "prosemirror-markdown";
+import { exampleSetup } from "prosemirror-example-setup";
 
-let $title = document.querySelector('#title')
-let $content = document.querySelector('#content')
-
+let $title = document.querySelector("#title");
 class ProseMirrorView {
-    constructor(target, content) {
-        this.view = new EditorView(target, {
-            state: EditorState.create({
-                doc: function(content) {
-                    // console.log('loading '+window.draftKey)
-                    let localDraft = localStorage.getItem(window.draftKey);
-                    if (localDraft != null) {
-                        content = localDraft
-                    }
-                    if (content.indexOf("# ") === 0) {
-                        let eol = content.indexOf("\n");
-                        let title = content.substring("# ".length, eol);
-                        content = content.substring(eol+"\n\n".length);
-                        $title.value = title;
-                    }
-                    return defaultMarkdownParser.parse(content)
-                }(content),
-                plugins: exampleSetup({schema})
-            }),
-            dispatchTransaction(transaction) {
-                // console.log('saving to '+window.draftKey)
-                $content.value = defaultMarkdownSerializer.serialize(transaction.doc)
-                localStorage.setItem(window.draftKey, function() {
-                    let draft = "";
-                    if ($title.value != null && $title.value !== "") {
-                        draft = "# "+$title.value+"\n\n"
-                    }
-                    draft += $content.value
-                    return draft
-                }());
-                let newState = this.state.apply(transaction)
-                this.updateState(newState)
-            }
-        })
+  constructor(target) {
+    let divContent = document.querySelector("#content");
+    const defaultContent = divContent.value;
+    const wordCounter = document.querySelector("#wc");
+    let content = "";
+    let localDraft = localStorage.getItem(window.draftKey);
+
+    if (localDraft) {
+      content = localDraft;
+      // setting initial word count
+      const wordCount = content.split(" ").filter((i) => i).length;
+      wordCounter.innerText = wordCount + " word" + (wordCount != 1 ? "s" : "");
     }
 
-    get content() {
-        return defaultMarkdownSerializer.serialize(this.view.state.doc)
+    // if no draft+PostId in the localStorage use hidden div.value as content
+    if (window.draftKey && window.draftKey != "lastDoc" && content == "") {
+      content = defaultContent;
     }
-    focus() { this.view.focus() }
-    destroy() { this.view.destroy() }
+
+    if (content.indexOf("# ") === 0) {
+      let eol = content.indexOf("\n");
+      let title = content.substring("# ".length, eol);
+      content = content.substring(eol + "\n\n".length);
+      $title.value = title;
+    }
+
+    const doc = defaultMarkdownParser.parse(content);
+
+    this.view = new EditorView(target, {
+      state: EditorState.create({
+        doc,
+        plugins: exampleSetup({ schema }),
+      }),
+      dispatchTransaction(transaction) {
+        const content = defaultMarkdownSerializer.serialize(transaction.doc);
+        // set hidden div for backup
+        divContent.value = content;
+
+        let draft = "";
+        if ($title.value != null && $title.value !== "") {
+          draft = "# " + $title.value + "\n\n";
+        }
+        draft += content;
+        localStorage.setItem(window.draftKey, draft);
+
+        //setting word counter value
+        const wordCount = draft.split(" ").filter((i) => i).length;
+        wordCounter.innerText =
+          wordCount + " word" + (wordCount != 1 ? "s" : "");
+
+        let newState = this.state.apply(transaction);
+        this.updateState(newState);
+      },
+    });
+  }
+
+  get content() {
+    return defaultMarkdownSerializer.serialize(this.view.state.doc);
+  }
+  focus() {
+    this.view.focus();
+  }
+  destroy() {
+    this.view.destroy();
+  }
 }
-
-let place = document.querySelector("#editor")
-let view = new ProseMirrorView(place, $content.value)
+let place = document.querySelector("#editor");
+setTimeout(() => {
+  const view = new ProseMirrorView(place);
+  window.editorInstance = view;
+}, 0);
 
 // document.querySelectorAll("input[type=radio]").forEach(button => {
 //   button.addEventListener("change", () => {
